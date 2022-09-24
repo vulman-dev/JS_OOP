451b4f158a21811734da7c0a88c07455e5cea1d2
Evgeniy <ezakharchev@yandex.ru>
2022-09-24 14:16:49 +0300

    Performing a new Time.js task

---
+++ Statistical properties and methods/Time.js
@@ -0,0 +1,17 @@
+export default class Time {
+    // BEGIN (write your solution here)
+    static fromString(time) {
+        const [hours, minutes] = time.split(':');
+        return new Time(hours, minutes);
+    }
+    // END
+
+    constructor(hours, minutes) {
+        this.minutes = minutes;
+        this.hours = hours;
+    }
+
+    toString() {
+        return `${this.hours}:${this.minutes}`;
+    }
+}
